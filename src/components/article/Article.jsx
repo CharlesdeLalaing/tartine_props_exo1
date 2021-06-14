9b94b6a0c2db672a6e3361f6110b277469be88e7
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-06-14 10:11:36 +0200

    push exo2 props

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -4,13 +4,15 @@ import './article.css'
 
 class Article extends React.Component {
 
-
     showTitle = () => {
         console.log(this.props.titre);
     }
 
+    nombreArticle = 42;
 
     render() {
+        console.log(this.props);
+
         return (
             <div className='article_div'>
                 <div className='article_div_left'>
@@ -21,6 +23,7 @@ class Article extends React.Component {
                     <h1>Mon Premier Article</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas qui, culpa earum ratione, accusantium adipisci asperiores tempore repellendus ullam temporibus, illo odit. Rem, debitis? Provident sequi recusandae non corrupti praesentium dolorem sed, temporibus numquam molestias eaque illo vitae doloribus nesciunt aspernatur deserunt reprehenderit. Quos architecto rem atque iure cumque. Eligendi!</p>
                     <button onClick={this.showTitle}>Presh me</button>
+                    <button onClick={() => this.props.retoursNombreArticle(this.nombreArticle)}>Combien d'articles?</button>
                 </div>
             </div>
         );
--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,11 +5,16 @@ import Article from '../article/Article'
 
 
 class Body extends React.Component {
+    
+    nbrArticles = (nbrArts) => {
+        console.log(nbrArts)
+    };
+
     render() {
         return (
             <div className='body_div'>
-                    <Article titre='Les tartines'/>
-                    <Article titre='CodingSchool'/>
+                    <Article titre='Les tartines' retoursNombreArticle={(num) => this.nbrArticles(num)}/>
+                    <Article titre='CodingSchool' retoursNombreArticle={(num) => this.nbrArticles(num)}/>
             </div>
         );
     }
